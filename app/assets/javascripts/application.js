b045777492b298fabd87d0f586d67ccc79c4f0f3
Bechir Segni <bechirsegni@gmail.com>
2016-01-24 00:15:53 +0100

    Added CSS and JS Files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,39 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require pace.min
 //= require jquery
 //= require jquery_ujs
+//= require modernizr.custom
+//= require jquery-1.11.1.min
+//= require bootstrap.min
+//= require jquery-easy
+//= require jquery.unveil.min
+//= require jquery.bez.min
+//= require jquery.ioslist.min
+//= require jquery.actual.min
+//= require jquery.scrollbar.min
+//= require select2.min
+//= require classie
+//= require switchery.min
+//= require d3.v3
+//= require nv.d3.min.js
+//= require tooltip
+//= require utils
+//= require interactiveLayer
+//= require axis
+//= require line
+//= require lineWithFocusChart
+//= require hammer
+//= require jquery.mousewheel
+//= require mapplic
+//= require rickshaw.min.js
+//= require MetroJs.min
+//= require jquery.sparkline.min
+//= require skycons
+//= require bootstrap-datepicker
+//= require pages.min
+//= require dashboard
+//= require scripts
+//= require demo
 //= require turbolinks
-//= require_tree .

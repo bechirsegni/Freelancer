1f18a2b4250e3e47d4278f6aa881b8d6187f84e6
Bechir Segni <bechirsegni@gmail.com>
2016-01-25 03:15:53 +0100

    ADD Dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title="")
+    base_tile="Freelancer Tunisia"
+    if page_title.blank?
+      base_tile
+    else
+      "#{page_title} | #{base_tile}"
+    end
+  end
 end

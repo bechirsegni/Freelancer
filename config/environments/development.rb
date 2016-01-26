a9f2979d3a8c5d926804ff2459e147337a470b43
Bechir Segni <bechirsegni@gmail.com>
2016-01-26 07:12:50 +0100

    Removing Live Reload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
-
   #For Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end

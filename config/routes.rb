56b3e0e323d3eaec9c3121c0d0804a39ce7f8ab3
Bechir Segni <bechirsegni@gmail.com>
2016-02-05 15:54:42 +0100

    Fixing Root For authenticated and unauthticated users and frontend controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,25 @@ Rails.application.routes.draw do
   end
   resources :showcases
   resources :articles
-  root to: 'static#dashboard'
     devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   devise_scope :user do
-    get 'login', :to => 'devise/sessions#new'
-    post 'login', :to => 'devise/sessions#new'
-    get 'register', :to => 'devise/registrations#new'
-    post 'register', :to => 'devise/registrations#new'
-    delete 'logout', to: 'devise/sessions#destroy'
-    get 'password', to: 'devise/passwords#new'
-    get 'account', to: 'devise/registrations#edit'
-    get "u/:id" => "users#show", :as => 'profile'
-    get 'freelancers', to: 'users#index'
+    get 'login',      :to => 'devise/sessions#new'
+    post 'login',     :to => 'devise/sessions#new'
+    get 'register',   :to => 'devise/registrations#new'
+    post 'register',  :to => 'devise/registrations#new'
+    delete 'logout',  to: 'devise/sessions#destroy'
+    get 'password',   to: 'devise/passwords#new'
+    get 'account',    to: 'devise/registrations#edit'
+    get 'u/:id' ,     to: 'users#show', :as => 'profile'
+    get 'freelancers',to: 'users#index'
+
+    authenticated :user do
+      root 'static#dashboard', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'frontend#index',    as: :unauthenticated_root
+    end
   end
 
   resources :categories
